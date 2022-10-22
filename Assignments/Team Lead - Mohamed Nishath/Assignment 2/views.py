f8ad765008f303e5b236d55c27bf9ecb40484428
nich-nichy <78273608+nich-nichy@users.noreply.github.com>
2022-10-22 19:59:21 +0530

    second commit

--- Assignments/Team Lead - Mohamed Nishath/Assignment 2/views.py
+++ Assignments/Team Lead - Mohamed Nishath/Assignment 2/views.py
@@ -1,23 +1,23 @@
 from flask import Blueprint, render_template
 
-views = Blueprint(__name__, "views")
+views = Blueprint('views', __name__)
 
 
-@views.route("/home")
+@views.route('/')
 def home():
-    return render_template("./templates/index.html")
+    return render_template("index.html")
 
 
-@views.route("/about")
+@views.route('/about')
 def about():
     return render_template("about.html")
 
 
-@views.route("/signin")
+@views.route('/signin')
 def signin():
     return render_template("signin.html")
 
 
-@views.route("/signup")
+@views.route('/signup')
 def signup():
     return render_template("signup.html")
