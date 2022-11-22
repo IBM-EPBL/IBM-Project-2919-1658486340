edbb67a7f18204aef81ef411db180171fb0b5c49
nich-nichy <78273608+nich-nichy@users.noreply.github.com>
2022-11-22 09:37:12 +0530

    third commit

--- Project Development Phase/Sprint 1/pet_App/app.py
+++ Project Development Phase/Sprint 1/pet_App/app.py
@@ -1,16 +1,14 @@
 from flask import Flask, render_template, request
-# from flask.ext.sqlalchemy import SQLAlchemy
-import logging
+from flask_sqlalchemy import SQLAlchemy
 from logging import Formatter, FileHandler
 from forms import *
-import os
 
 
 app = Flask(__name__)
 app.config.from_object('config')
 
 
-# db = SQLAlchemy(app)
+db = SQLAlchemy(app)
 
 
 # Views and routes
@@ -48,48 +46,14 @@ def forgot():
     form = ForgotForm(request.form)
     return render_template('forms/forgot.html', form=form)
 
+
 # Sql performances
 
 
-# @app.teardown_request
-# def shutdown_session(exception=None):
-#     db_session.remove()
+def login_required(test):
+    pass
 
 
-# def login_required(test):
-#     @wraps(test)
-#     def wrap(*args, **kwargs):
-#         if 'logged_in' in session:
-#             return test(*args, **kwargs)
-#         else:
-#             flash('You need to login first.')
-#             return redirect(url_for('login'))
-#     return wrap
-
-
-# Error handling routes.
-
-@app.errorhandler(500)
-def internal_error(error):
-    # db_session.rollback()
-    return render_template('errors/500.html'), 500
-
-
-@app.errorhandler(404)
-def not_found_error(error):
-    return render_template('errors/404.html'), 404
-
-
-if not app.debug:
-    file_handler = FileHandler('error.log')
-    file_handler.setFormatter(
-        Formatter(
-            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
-    )
-    app.logger.setLevel(logging.INFO)
-    file_handler.setLevel(logging.INFO)
-    app.logger.addHandler(file_handler)
-    app.logger.info('errors')
 # Run file
 if __name__ == '__main__':
     app.run()
--- Project Development Phase/Sprint 1/pet_App/config.py
+++ Project Development Phase/Sprint 1/pet_App/config.py
@@ -2,6 +2,6 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
-SECRET_KEY = 'MwLL3HQxmG'
+SECRET_KEY = ''
 # Connect to the database
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'name.db')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'registration.db')
